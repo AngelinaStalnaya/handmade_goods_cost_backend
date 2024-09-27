2daf4ce7b90b53a29cfb314f4cfab54944022cc9
Angelina Stalnaya <angelinastalnaya.feb@gmail.com>
2024-09-27 12:53:03 +0300

    get user added

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const PORT = 7000;
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send('Hello from our serverrrr!')
+    res.send('Hello from our server!')
+})
+app.get('/get_user', (req,res) => {
+    res.send('This is user data')
 })
 
 app.listen(PORT, () => {
