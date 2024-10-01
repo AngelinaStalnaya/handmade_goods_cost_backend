8aed077541cd1f95bcc208f2790370b789046c62
Angelina Stalnaya <angelinastalnaya.feb@gmail.com>
2024-10-01 16:24:53 +0300

    calculation router - get user calculations- changed

--- routers/calculationRouter.js
+++ routers/calculationRouter.js
@@ -3,7 +3,7 @@ import CalculationController from "../controllers/CalculationController.js";
 const calculationRouter = new Router();
 
 calculationRouter.post("/calculations", CalculationController.create);
-calculationRouter.get("/calculations/:id", CalculationController.getAll);
+calculationRouter.get("/calculations/user/:id", CalculationController.getAll);
 calculationRouter.get('/calculations/:id', CalculationController.getOne)
 calculationRouter.put('/calculations/:id', CalculationController.update)
 calculationRouter.delete('/calculations/:id', CalculationController.delete)
